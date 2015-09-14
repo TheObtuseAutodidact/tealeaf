1e6cb17635e287c5dcdec9eff0bbf4ffec41e743
Obtuse Autodidact <theobtuseautodidact@gmail.com>
2015-09-14 13:16:28 -0600

    quiz_3.rb

---
+++ tealeaf_ruby_workbook/quiz_3.rb
@@ -0,0 +1,47 @@
+#quiz_3.rb
+
+#1. Show an easier way to write this array:
+
+#flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+puts flintstones.inspect
+
+#2. How can we add the family pet "Dino" to our usual array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+puts flintstones.inspect
+
+#3. In the previous exercise we added Dino to our array like this:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+#We could have used either Array#concat or Array#push to add Dino to the family.
+
+#How can we add multiple items to our array? (Dino and Hoppy)
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.push(["Dino", "Hoppy"]).flatten!
+puts flintstones.inspect
+
+#4. Shorten this sentence:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+#...remove everything starting from "house".
+
+#Review the String#slice! documentation, and use that method to make the return value "Few things in life are as important as ". But leave the advice variable as "house training your pet dinosaur.".
+puts advice.slice!(0, advice.index('house'))
+#As a bonus, what happens if you use the String#slice method instead?
+
+#5. Write a one-liner to count the number of lower-case 't' characters in the following string:
+
+statement = "The Flintstones Rock!"
+
+puts statement.split("").select{|char| char == "t"}.length
+
+puts statement.scan('t').size
+
+#6. Back in the stone age (before CSS) we used spaces to align things on the screen. If we had a 40 character wide table of Flintstone family members, how could we easily center that title above the table with spaces?
+
+title = "Flintstone Family Members"
+puts title.center(40) 
